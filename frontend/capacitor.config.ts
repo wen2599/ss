010aaaa47b2d93ad7df8e97752f3102c005b7aec
wen2599 <87487280+wen2599@users.noreply.github.com>
2025-09-14 18:49:38 +0800

    Delete frontend/capacitor.config.ts

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.thirteen',
-  appName: '十三张',
-  webDir: 'build'
-};
-
-export default config;
