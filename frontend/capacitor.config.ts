c6bbf4d0a3d3bd665916c551a5adf0302fd2d708
huangjie2599 <huangjie2599@gmail.com>
2025-09-14 16:24:13 +0000

    修复完善

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.lll',
+  appName: '资料',
+  webDir: 'dist'
+};
+
+export default config;
