8459ea9b09e276f0819de01afa098ac54ac0f95d
junwuji717 <junwuji717@gmail.com>
2025-10-09 06:14:33 +0000

    fix: Unify database environment variables in setup_database.php

--- backend/setup_database.php
+++ backend/setup_database.php
@@ -6,9 +6,9 @@ require_once __DIR__ . '/src/core/DotEnv.php';
 
 // Database credentials from environment variables
 $servername = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
