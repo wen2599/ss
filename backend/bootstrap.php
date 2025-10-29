81290a4ee152b48fcdc39a297bf3595cea1ad752
junwuji717 <junwuji717@gmail.com>
2025-10-29 10:58:16 +0000

    Fix CORS issue in backend/bootstrap.php

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -12,6 +12,7 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
     header("Access-Control-Allow-Origin: " . $allowed_origin);
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(204);
