d4d3125a2658253d3269c2ed2d6a3001471e4238
junwuji717 <junwuji717@gmail.com>
2025-10-31 03:56:43 +0000

    Update all project files

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // 你可能需要创建一个空的 index.css 文件
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
