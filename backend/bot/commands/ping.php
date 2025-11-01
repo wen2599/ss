a344cedeed6ffa071fd9203688314ae74654dcf0
wen2599 <87487280+wen2599@users.noreply.github.com>
2025-11-02 00:55:01 +0800

    Delete backend directory

--- backend/bot/commands/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-// backend/bot/commands/ping.php
-
-require_once __DIR__ . '/../bot_helpers.php';
-
-function handle_command($bot_token, $chat_id, $message) {
-    $text = "pong";
-    send_message($bot_token, $chat_id, $text);
-}
