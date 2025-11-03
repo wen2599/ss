cdcc29523d380368a6328282c5c315c50f29a2cc
junwuji717 <junwuji717@gmail.com>
2025-11-03 09:23:15 +0000

    Changes before Firebase Studio auto-run

--- frontend/android/app/src/main/java/com/example/lotterymanagement/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.lotterymanagement;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/src/pages/SettlementsPage.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const SettlementsPage = () => <h1>结算清单</h1>;
-export default SettlementsPage;
